4bcba26cc0f2da0014f61bc0b49bacf759eb99bd
surkova <surkov@company.moisport.ru>
2024-11-04 17:43:54 +0700

    hw-13

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/api/GetEmptyUserListTest.java
@@ -0,0 +1,29 @@
+package guru.qa.niffler.test.api;
+
+import guru.qa.niffler.api.UserdataApi;
+import guru.qa.niffler.api.core.RestClient;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.model.UserJson;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Order(1)
+public class GetEmptyUserListTest {
+
+    private static final Config CFG = Config.getInstance();
+    private final UserdataApi userdataApi = new RestClient.EmptyClient(CFG.userdataUrl()).create(UserdataApi.class);
+
+    @User
+    @Test
+    void getEmptyUsersListTest(UserJson user) throws IOException {
+        List<UserJson> allUsers = userdataApi.allUsers(user.username(), "").execute().body();
+        assertThat(allUsers).isEmpty();
+    }
+
+}
