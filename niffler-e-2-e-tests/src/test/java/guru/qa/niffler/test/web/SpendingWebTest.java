05a389d99dd5f9e5f929048a6ebfa0cefdd0a471
surkova <surkov@company.moisport.ru>
2024-11-06 09:26:46 +0700

    lesson 9.1 stream 17

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -1,14 +1,21 @@
 package guru.qa.niffler.test.web;
 
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.UserJson;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Date;
 
+import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.utils.RandomDataUtils.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SpendingWebTest extends TestBaseWeb {
 
@@ -49,6 +56,24 @@ public class SpendingWebTest extends TestBaseWeb {
         newSpendingPage.setDescription(description);
         newSpendingPage.clickAddButton();
         mainPage.getSpendingTable().checkTableContains(description);
+    }
+
+    @User(
+            spendings = @Spending(
+                    category = "Обучение",
+                    description = "Обучение Advanced 2.0",
+                    amount = 79990
+            )
+    )
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        loginPage.login(user.username(), user.testData().password());
+
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 
 }
