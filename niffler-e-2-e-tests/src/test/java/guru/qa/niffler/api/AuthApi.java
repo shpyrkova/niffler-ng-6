d6e574f9f6336a05fac3e9a9e445de3b128dc81e
surkova <surkov@company.moisport.ru>
2024-11-25 16:27:46 +0700

    hw-18

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -11,7 +12,7 @@ import retrofit2.http.Query;
 public interface AuthApi {
 
     @GET("oauth2/authorize")
-    Call<Void> authorize(
+    Call<Response<Void>> authorize(
             @Query("response_type") String responseType,
             @Query("client_id") String clientId,
             @Query("scope") String scope,
