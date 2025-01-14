7bac4c273358dbaff14271c94c866136fb07fba1
surkova <surkov@company.moisport.ru>
2025-01-14 15:10:42 +0700

    cherry pick fix

--- niffler-gateway/src/main/java/guru/qa/niffler/config/NifflerGatewayServiceConfig.java
+++ niffler-gateway/src/main/java/guru/qa/niffler/config/NifflerGatewayServiceConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -28,7 +29,8 @@ public class NifflerGatewayServiceConfig {
   private final String nifflerGatewayBaseUri;
 
   @Autowired
-  public NifflerGatewayServiceConfig(@Value("${niffler-userdata.base-uri}") String nifflerUserdataBaseUri) {
+  public NifflerGatewayServiceConfig(@Value("${niffler-userdata.base-uri}") String nifflerUserdataBaseUri,
+                                     @Value("${niffler-gateway.base-uri}") String nifflerGatewayBaseUri) {
     this.nifflerUserdataBaseUri = nifflerUserdataBaseUri;
     this.nifflerGatewayBaseUri = nifflerGatewayBaseUri;
   }
@@ -60,17 +62,17 @@ public class NifflerGatewayServiceConfig {
     Server server = new Server();
     server.setUrl(nifflerGatewayBaseUri);
     return new OpenAPI()
-        .servers(List.of(server))
-        .info(new Info()
-            .title("Niffler Gateway API Documentation")
-            .version("1.0")
-            .description("API documentation with Swagger and SpringDoc"))
-        .addSecurityItem(new SecurityRequirement().addList(OPEN_API_AUTH_SCHEME))
-        .components(new Components()
-            .addSecuritySchemes(OPEN_API_AUTH_SCHEME, new SecurityScheme()
-                .name(OPEN_API_AUTH_SCHEME)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")));
+            .servers(List.of(server))
+            .info(new Info()
+                    .title("Niffler Gateway API Documentation")
+                    .version("1.0")
+                    .description("API documentation with Swagger and SpringDoc"))
+            .addSecurityItem(new SecurityRequirement().addList(OPEN_API_AUTH_SCHEME))
+            .components(new Components()
+                    .addSecuritySchemes(OPEN_API_AUTH_SCHEME, new SecurityScheme()
+                            .name(OPEN_API_AUTH_SCHEME)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")));
   }
 }
